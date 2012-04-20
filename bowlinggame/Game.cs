bb0e86de68512054a233fd4bb6de22f84fce2f9e
wumengxian <969903420@qq.com>
2012-04-20 11:16:04 +0800

    first commit

---
+++ bowlinggame/Game.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BowlingTest
+{
+    class Game
+    {
+           
+        private int currentFrame = 0;  
+        private bool isFirstThrow = true;
+        private Scorer scorer = new Scorer();
+
+        public int Score
+        {
+            get { return ScoreForFrame(currentFrame ); }
+        }
+
+        public void Add(int pins)
+        {
+            scorer.AddThrow(pins);           
+            AdjustCurrentFrame(pins);
+        }
+
+        private void AdjustCurrentFrame(int pins)
+        {
+            if (LastBallInFrame(pins))
+                    AdvanceFrame();
+              else
+                    isFirstThrow = false;                                
+        }
+
+        private bool LastBallInFrame(int pins)
+        {
+            return Strike(pins)||(!isFirstThrow);
+        }
+
+        private bool AdjustFrameForStrike(int pins)
+        {
+            if (Strike(pins)||(!isFirstThrow))
+            {
+                AdvanceFrame();
+            }
+            return false;
+       }
+
+        private bool Strike(int pins)
+        {
+            return (isFirstThrow && pins == 10);
+        }
+
+        private void AdvanceFrame()
+        {
+            currentFrame++;
+            if (currentFrame > 10)
+                currentFrame = 10;
+
+        }
+        
+        public int ScoreForFrame(int theFrame)
+        {
+            return scorer.ScoreForFrame(theFrame);
+        }
+    }
+}
+    
+  
+        
+      
---
+++ bowlinggame/GameTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingTest
+{
+    [TestClass]
+    public class GameTest
+    {
+        private static Game SetUp()
+        {
+            Game game = new Game ();
+            return game;
+        }
+        [TestMethod]
+        public void TestTwoThrowNoMark()
+        {
+            Game game = SetUp();
+            game.Add(5);
+            game.Add(4);
+            Assert.AreEqual(9, game.Score);
+         }
+        [TestMethod]
+        public void TestFourThrowNoMark()
+        {
+            Game game = SetUp();
+            game.Add(5);
+            game.Add(4);
+            game.Add(7);
+            game.Add(2);
+            Assert.AreEqual(18, game.Score);
+            Assert.AreEqual(9, game.ScoreForFrame(1));
+            Assert.AreEqual(18, game.ScoreForFrame(2));
+          
+        }
+        [TestMethod ]
+    public void TestSimpleSpare()
+    {
+        Game game = SetUp();
+        game.Add(3);
+        game.Add(7);
+        game.Add(3);
+        Assert.AreEqual(13, game.ScoreForFrame(1));
+     
+    }
+        [TestMethod]
+        public void TestSimpleFrameAfterSpare()
+        {
+            Game game = SetUp();
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            game.Add(2);
+            Assert.AreEqual(13, game.ScoreForFrame(1));
+            Assert.AreEqual(18, game.ScoreForFrame(2));
+            Assert.AreEqual(18,game.Score);
+
+        }
+        [TestMethod]
+        public void TestSimpleStrike()
+        {
+            Game game = SetUp();
+            game.Add(10);
+            game.Add(3);
+            game.Add(6);
+            Assert.AreEqual(19,game.ScoreForFrame(1));
+            Assert.AreEqual(28, game.Score);
+        }
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            Game game = SetUp();
+            for (int i = 0; i < 12; i++)
+            {
+                game.Add(10);
+            }
+            Assert.AreEqual(300, game.Score);
+        }
+        [TestMethod]
+        public void TestEndofArray()
+        {
+            Game game = SetUp();
+            for (int i = 0; i < 9;i++ )
+            {
+                game.Add(0);
+                game.Add(0);
+            }
+            game.Add(2);
+            game.Add(8);
+            game.Add(10);
+            Assert.AreEqual(20, game.Score);
+        }
+        [TestMethod]
+
+        public void TestSampleGame()
+        {
+            Game game = SetUp();
+            game.Add(1);
+            game.Add(4);
+            game.Add(4);
+            game.Add(5);
+            game.Add(6);
+            game.Add(4);
+            game.Add(5);
+            game.Add(5);
+            game.Add(10);
+            game.Add(0);
+            game.Add(1);
+            game.Add(7);
+            game.Add(3);
+            game.Add(6);
+            game.Add(4);
+            game.Add(10);
+            game.Add(2);
+            game.Add(8);
+            game.Add(6);
+            Assert.AreEqual(133, game.Score);
+        }
+        [TestMethod]
+        public void TestHeartBreak()
+        {
+            Game game=SetUp();
+            for (int i = 0; i < 11; i++)
+                game.Add(10);
+            game.Add(9);
+            Assert.AreEqual(299, game.Score);             
+        }
+        [TestMethod]
+        public void TestTenthFrameSpare()
+        {
+            Game game = SetUp();
+            for (int i = 0; i < 9; i++)
+                game.Add(10);
+            game.Add(9);
+            game.Add(1);
+            game.Add(1);
+            Assert.AreEqual(270, game.Score);
+        }
+    }
+}
+
+
